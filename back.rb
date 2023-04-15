2e50114de181c93bf3d54d3f6e355962c873e5c7
EC2 Default User <ec2-user@ip-172-31-37-122.ap-northeast-1.compute.internal>
2023-04-15 09:57:51 +0000

    a

--- back.rb
+++ back.rb
@@ -1,10 +1,28 @@
-i = 1
-while i <= 10 do
-  if i == 11
-    puts "処理を終了しま"
-    break  # iが5になると繰り返しから抜ける
+def solution(s)
+  #solutionを定義
+  stack = []
+  #stackを初期化
+  s.each_char do |char|
+    #文字列Sを１文字ずつ処理するためeachを使いループさせる
+    case char
+    when '(', '[', '{'
+      stack.push(char)
+      #もし開き括弧ならstackにCHARを追加
+    when ')'
+      return false unless stack.pop == '('
+    #もしとじ括弧ならstackの要素を取り出し対応しているかを確認。していなかったらfalse
+    when ']'
+      return false unless stack.pop == '['
+     #もしとじ括弧ならstackの要素を取り出し対応しているかを確認。していなかったらfalse
+    when '}'
+      return false unless stack.pop == '{'
+    #もしとじ括弧ならstackの要素を取り出し対応しているかを確認。していなかったらfalse
+    end
+    #ループを終了
   end
-  puts i
-  i += 1 # iの数値に1を加えたい時は、i = i +1と書く代わりに、i += 1と書くことができます。
+  stack.empty?
+  #stackがからであるかを確認し空ならtrue
 end
 
+puts solution("{")
+  
